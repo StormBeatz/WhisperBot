18a0a544b994a7d258056ab428a7d9abc692c3fa
𝐆𝐫𝐚𝐲𝐁𝐨𝐭𝐬 <118429184+StormBeatz@users.noreply.github.com>
2025-05-01 07:53:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 # Pyrogram setup
 API_ID = "28771411"  # Replace this API ID with your actual API ID
 API_HASH = "a83f7f4d63d3b583b1d467c79c39de65"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "7573252812:AAHgEciRa6-cDfS8FHqUFvkhClHew1KV0iA"  # Replace this BOT_TOKEN
+BOT_TOKEN = "8190024655:AAGrUNPElcGT5QcHxuueW_g0bjoTf3q3yaU"  # Replace this BOT_TOKEN
 
 # Bot USER_NAME
-BOT_USERNAME = "ssstbbot" # Dont Use @ Just Put the bot username
+BOT_USERNAME = "PpptRobot" # Dont Use @ Just Put the bot username
